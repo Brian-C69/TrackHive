319e30d5f42577fd8185ea987bc65bc84a72b59f
Bernard.C <bchoong1@gmail.com>
2025-09-17 11:49:02 +0800

    new edit

---
+++ Models/20250917033921_MergeFix.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackHive.Models
+{
+    /// <inheritdoc />
+    public partial class MergeFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PayrollRecords_UserId",
+                table: "PayrollRecords");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_PayrollRecords_UserId",
+                table: "PayrollRecords",
+                column: "UserId");
+        }
+    }
+}
