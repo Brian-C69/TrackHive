3ff2e7925684e0d1bd84e03ff0351b1da5cf570a
Bernard.C <bchoong1@gmail.com>
2025-09-17 11:25:48 +0800

    update db

---
+++ Models/20250916142024_Sync_20250916.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrackHive.Models
+{
+    /// <inheritdoc />
+    public partial class Sync_20250916 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AttendanceRecords_UserId",
+                table: "AttendanceRecords");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AttendanceRecords_UserId",
+                table: "AttendanceRecords",
+                column: "UserId");
+        }
+    }
+}
